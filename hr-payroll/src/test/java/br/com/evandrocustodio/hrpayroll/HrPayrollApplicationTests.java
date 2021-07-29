bdc8e45e24907a351435ee9703e942cf45a90192
EVANDRO CUSTODIO GONCALVES - GETIC <208396@int.cagece.com.br>
2021-07-28 23:07:31 -0300

    Criando Projeto HrPayrol

---
+++ hr-payroll/src/test/java/br/com/evandrocustodio/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evandrocustodio.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
