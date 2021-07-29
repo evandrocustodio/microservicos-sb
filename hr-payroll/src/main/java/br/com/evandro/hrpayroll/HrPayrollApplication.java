be724697860a3cb6db611edd94eff688d6885683
EVANDRO CUSTODIO GONCALVES - GETIC <208396@int.cagece.com.br>
2021-07-28 23:56:01 -0300

    Consumindo Serviço Workers no Payments Service atraves de FeignClients

--- hr-payroll/src/main/java/br/com/evandro/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/evandro/hrpayroll/HrPayrollApplication.java
@@ -2,13 +2,13 @@ package br.com.evandro.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HrPayrollApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HrPayrollApplication.class, args);
 	}
-
 }
 	
--- hr-payroll/src/main/java/br/com/evandro/hrpayroll/controllers/PayrollController.java
+++ hr-payroll/src/main/java/br/com/evandro/hrpayroll/controllers/PayrollController.java
@@ -1,17 +1,13 @@
 package br.com.evandro.hrpayroll.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.evandro.hrpayroll.feignclients.WorkerFeignClient;
 import br.com.evandro.hrpayroll.models.Payment;
 import br.com.evandro.hrpayroll.models.Worker;
 import br.com.evandro.hrpayroll.services.PaymentService;
@@ -22,23 +18,17 @@ public class PayrollController {
 
 	@Autowired
 	private PaymentService service;
-	
-	@Autowired
-	private RestTemplate rt;
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
-	@GetMapping(value = "/{workerid}/days/{days}")
-	public ResponseEntity<Payment> getPayment(@PathVariable Long workerid, @PathVariable Integer days) {		
-		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", workerid.toString());
 
-		Worker worker = rt.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
-		
-		
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
+	@GetMapping(value = "/{workerid}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerid, @PathVariable Integer days) {
+
+		Worker worker = workerFeignClient.getWorkerById(workerid).getBody();
+
 		Payment payment = this.service.getPayment(worker, days);
+		
 		return ResponseEntity.ok(payment);
 
 	}
